ed60b323300de4534854e6ed09daa35d76a337c5
bienk <sb2394@njit.edu>
2019-04-01 05:18:38 -0400

    Task: First commit

---
+++ resources/views/pages/about.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.landing')
+@section('content')
+
+    <h1>Hello World, Welcome to my About Page</h1>
+
+@stop
---
+++ resources/views/pages/home.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.landing')
+@section('content')
+
+    <h1>Hello World, Welcome to my Homepage</h1>
+
+@stop
